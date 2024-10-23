9062da93951b1a86e424d5744aa3e451a3281775
ashishkumar739293 <ashishkumar739293@gmail.com>
2024-10-23 09:06:20 +0530

    first commit

---
+++ resources/js/components/ExampleComponent.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ExampleComponent = () => {
+    return (
+        <div>
+            <h1>Hello, my name is Ashish, and this is a React component!</h1>
+        </div>
+    );
+};
+
+export default ExampleComponent;
